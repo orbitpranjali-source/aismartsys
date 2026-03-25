3c69dd64f5cc7471dc03b39f15eaea2db8001c0c
orbitpranjali-source <orbitpranjali@gmail.com>
2026-03-25 16:47:12 +0530

    Error solve in website running

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter basename={import.meta.env.PROD ? "/aismartsys/" : "/"}>
+          <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/about" element={<About />} />
